d3324d46081bb92bb1053e096bd84fad27900a01
Ruslan Jevik <47752953+ijevius@users.noreply.github.com>
2019-06-29 20:16:15 +0300

    Update TFSSummer2019Ex2.py

--- TFSSummer2019Ex2.py
+++ TFSSummer2019Ex2.py
@@ -1,22 +1,22 @@
 s = input()
-orig = ""
 l = len(s)
+orig = ''
 
 if l % 2 == 0:
     even = int(l/2)
     odd = 0
     while even != l:
-        orig = orig + str(s[even]) + str(s[odd])
-        even = even+1
-        odd = odd+1
+        orig += str(s[even]) + str(s[odd])
+        even += 1
+        odd += 1
 else:
     odd = int(l / 2)
     even = 0
     while odd != l:
+        orig = orig + str(s[odd])
         if odd != l-1:
-            orig = orig + str(s[odd]) + str(s[even])
-        else:
-            orig = orig + str(s[odd])
-        even = even + 1
-        odd = odd + 1
+            orig += str(s[even])
+        even += 1
+        odd += 1
+        
 print(orig)
